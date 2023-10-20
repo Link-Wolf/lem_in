f9376ff2507007579d3bc2935c9ff6cac250780c
iCARUS <94838879+sur4c1@users.noreply.github.com>
2023-10-20 11:23:54 +0200

    add error messages more complete
    Co-authored-by: Link <Link-Wolf@users.noreply.github.com>

--- srcs/utils/bugs.c
+++ srcs/utils/bugs.c
@@ -3,18 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   bugs.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: link <link@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: iCARUS <iCARUS@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 14:20:02 by event             #+#    #+#             */
-/*   Updated: 2023/10/18 15:40:50 by link             ###   ########.fr       */
+/*   Updated: 2023/10/20 11:23:54 by iCARUS           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/utils.h"
 
+const char	*error_msgs[] = {
+	[OK] = "OK",
+	[ERR_ROOM_ALREADY_EXISTS] = "Rooms cannot share the same name",
+	[ERR_ALLOCATION] = "Cannot allocate memory",
+	[ERR_ROOM_TYPE_CONFLICT] = "Room cannot be EXIT and ENTRY at the same time",
+	[ERR_CMD_ROOM] = "Commands must be before a valide room declaration",
+	[ERR_VALUE_ANTS] = "Ant value must be a valid positive integer",
+	[ERR_ROOM_SYNTAX] = "Room declarations must follow the appropriate syntax",
+	[ERR_SELF_LINKING_ROOM] = "Links cannot link a room to it self",
+	[ERR_ROOM_DOES_NOT_EXIST] = "Links cannot link to unexisting rooms",
+	[ERR_LINK_SYNTAX] = "Link declarations must follow the appropriate syntax",
+	[ERR_NO_LINKS] = "The map is missing links",
+	[ERR_CMD_LINK] = "Commands cannot be followed by links"
+};
+
 void	bugs(t_lem_in *lem_in, int code)
 {
-	ft_printf("ERROR: %d\n", code);
+	ft_printf("ERROR %d: %s\n", code, error_msgs[code]);
 	tini(lem_in);
 	exit(code);
 }
